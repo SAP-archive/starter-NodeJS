116bbcd3059056ebbf43ec2f226d6ec5471306cf
jhoudan <jerom.houdan@gmail.com>
2017-01-06 15:22:54 +0100

    [ADD] Mongoose dependency

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
-
 import Bot from 'recastai-botconnector'
 
-import config from '../config/index.js'
 import {
   BotConnectorConfig,
-} from '../config/index.js'
+} from '../config/index'
+
 
 // BotConnector setup
 const bot = new Bot(BotConnectorConfig)
+
 
 // Server setup
 const app = express()
@@ -17,12 +18,26 @@ app.set('port', config.port || 5000)
 app.use(bodyParser.json())
 app.use('/', (req, res) => bot.listen(req, res))
 
+
 // When the bot receive a message...
 bot.onTextMessage(message => {
   console.log(message)
 })
 
+
 // MongoDB connection
-app.listen(app.get('port'), () => {
-  console.log('Our bot is running on port', app.get('port'))
+let db = 'mongodb://'
+if (config.database.username) { db = `${db}${config.database.username}:${config.database.password}@` }
+db = `${db}${config.database.hostname}:${config.database.port}/${config.database.name}`
+if (config.database.ssl) { db = `${db}?ssl=${config.database.ssl}` }
+mongoose.connect(db, err => {
+  if (err) {
+    console.error('An error occured while connecting to MongoDB')
+    throw err
+  }
+
+  app.listen(app.get('port'), () => {
+    console.log('Our bot is running on port', app.get('port'))
+  })
 })
+
