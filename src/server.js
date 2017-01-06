69c0be433d2268707138b9f572aa4a26898afcc7
jhoudan <jerom.houdan@gmail.com>
2017-01-06 15:18:27 +0100

    [UPD] refacto server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 import express from 'express'
-import config from '../config/index.js'
 import bodyParser from 'body-parser'
 
+import Bot from 'recastai-botconnector'
+
+import config from '../config/index.js'
+import {
+  BotConnectorConfig,
+} from '../config/index.js'
+
+// BotConnector setup
+const bot = new Bot(BotConnectorConfig)
+
+// Server setup
 const app = express()
 app.set('port', config.port || 5000)
 app.use(bodyParser.json())
+app.use('/', (req, res) => bot.listen(req, res))
 
+// When the bot receive a message...
+bot.onTextMessage(message => {
+  console.log(message)
+})
+
+// MongoDB connection
 app.listen(app.get('port'), () => {
   console.log('Our bot is running on port', app.get('port'))
 })
